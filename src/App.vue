d544d7efe043f0055b65f45fc953e5c9b31f4f15
rosario <58920070+athena272@users.noreply.github.com>
2026-02-24 16:59:35 -0300

    feat: adicionando análise de dados, de acessos a página pela Vercel

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import { Analytics } from '@vercel/analytics/vue';
 import AppLayout from '@/components/AppLayout.vue'
 </script>
 
 <template>
+  <Analytics />
   <AppLayout>
     <RouterView />
   </AppLayout>
